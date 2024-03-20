ccd470a959f745b8204511d4f556e580b5167ec9
Addieryn <aarondehmlow@yahoo.com>
2024-03-20 13:25:59 -0700

    commenting and cleaning code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,80 +3,71 @@ import os
 import csv
 
 
-budget_data = []
-total_month = 0
-
 Resource = os.path.join('Resources', 'budget_data.csv')
 
-print("Financial Analysis")
-print("-------------------")
 
+budget_data = [] #has to be list because its tracking the whole row
+total_month = 0 #creating values outside read/loops
 total = 0
 pl_line = 0
-month_change = []
+month_change = [] #has to be list because it keeps track of its relative place in the row so it can save all values
 list_change = []
-increase = ["", 0]
+increase = ["", 0]         #list because multiple comparable values ["" for the value of the row, 0 for the comparison to the row]
 decrease = ["",999999999]
 
-with open(Resource, encoding='UTF-8') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(Resource) as csvfile:     #finding and reading csv
+    csvreader = csv.reader(csvfile)
 
-    csv_header = next(csvreader)
+    csv_header = next(csvreader) #skipping header
     budget_data.append(csv_header)
    
-    profit_loss = 0
-    first_row = next(csvreader)
-    total_month += 1
-    total += int(first_row[1])
-    pl_line += int(first_row[1])    
-  
+    start_row = next(csvreader) #starts at row after header
+   
+    total_month = 1             #starting value 
+    total = int(start_row[1])   
+    pl_line = int(start_row[1])    
+    
 
     for row in csvreader:
         
-        
-        total_month += 1
-        month = str(row[0]) 
-        profit_loss += int(row[1]) 
-       
+        total_month += 1        #taking value and adding to the next
         total += int(row[1])
 
-        net_change = int(row[1]) - pl_line
+        net_change = int(row[1]) - pl_line # - because we need change(negative numbers)
         
-        pl_line = int(row[1])
-        list_change += [net_change]
+        pl_line = int(row[1])       #pl_line comes after net_change to set new value and not override
+        
+        list_change += [net_change] #adding the changes together 
         month_change += [row[0]]
 
-
-        if net_change < decrease[1]:
-            decrease[0] = row[0]
-            decrease[1] = net_change
-
+        if net_change < decrease[1]: #taking each rows change value and comparing it to our list number 0
+            decrease[0] = row[0]     #making our list value equal to row value
+            decrease[1] = net_change #making our number we are comparing the next iteration
         if net_change > increase[1]:
             increase[0] = row[0]
-            increase[1] = net_change
-        
+            increase[1] = net_change  
   
-         
-def average(list_change):
-    length = len(list_change)
-    total = sum(list_change)
-    return total / length
+#def needs to be out of loop but doesnt matter where       
+def average(list_change):       #using list_change because its the list of all numbers in column
+    length = len(list_change)   #number of values
+    total = sum(list_change)    #total of values
+    return total / length       #return sends the value calculated
 
-  
+print("Financial Analysis")
+print("-------------------")  
 print(f"Total Months: {total_month}")
-print(f"Total: ${profit_loss}")
-print(f"Average Change: ${average(list_change):.2f}")
-print(f"Greatest increase in profit: {increase[0]} (${increase[1]})")
+print(f"Total: ${total}")
+print(f"Average Change: ${average(list_change):.2f}")           #using .2f rounds to 2 decimals
+print(f"Greatest increase in profit: {increase[0]} (${increase[1]})")   #using [0] and [1] for the month captured in list and value accociated with it
 print(f"Greatest decrease in profit: {decrease[0]} (${decrease[1]})")
 
-#output_path = os.path.join("analysis", "Financial_Analysis.csv")
+output_path = os.path.join("analysis", "Financial_Analysis.csv")
 
-#with open(output_path, 'w') as csvfile:
-      #csvwriter = csv.writer(csvfile)
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=",")
       
-        #csvwriter.writerow(f"Total Months: {total_month}")
-        #csvwriter.writerow(f"Total: ${profit_loss}")
-        #csvwriter.writerow(f"Average Change: ${average(list_change):.2f}")
-        #csvwriter.writerow(f"Greatest increase in profit: {increase[0]} (${increase[1]})")
-        #csvwriter.writerow(f"Greatest decrease in profit: {decrease[0]} (${decrease[1]})")
-     
+    csvwriter.writerow(["Total Months:", total_month])
+    csvwriter.writerow(["Total:", f"${total}"])                                 #using f" to take value as is but still seperating using ","
+    csvwriter.writerow(["Average Change:", f"${average(list_change):.2f}"])
+    csvwriter.writerow(["Greatest increase in profit:", increase[0], f"${increase[1]}"])
+    csvwriter.writerow(["Greatest decrease in profit:", decrease[0], f"${decrease[1]}"])
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # python-challenge
 Help from Tutor Khangwelo with finding my Change variables in the code next_change[], and printing month of line for increase[0] and decrease[0]
     Also asked about an easy way to round down using .2f
+Xpert showed me how to seperate the csv output without a "," after every letter
+    (I didn't realize I could use an f string in the middle and not just the beginning)
