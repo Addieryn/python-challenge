cdb15eb30ab43a8c63b4d03732330c26d1dd9e25
Addieryn <aarondehmlow@yahoo.com>
2024-03-21 12:47:33 -0700

    commenting and cleaning code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,21 +2,10 @@
 import os
 import csv
 
-
-#A complete list of candidates who received votes
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#The winner of the election based on popular vote
-
-election_data = []
+election_data = []  #creating where i'll store info
 total_ballet = 0
-can_list = []
-first_can = ["", []]
-second_can = ["", []]
-third_can = ["", []]
 
-Resource = os.path.join('Resources', 'election_data.csv')
-
+Resource = os.path.join('Resources', 'election_data.csv') #finding and reading csv
 
 with open(Resource, encoding='UTF-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -24,59 +13,57 @@ with open(Resource, encoding='UTF-8') as csvfile:
     csv_header = next(csvreader)
     election_data.append(csv_header)
 
-    total_ballet = 0
+    total_ballet = 0        #setting up values
     vote = 0
 
-    candidate_dict = {}
+    candidate_dict = {}     #dictionary to hold lists of pulled info
 
     for row in csvreader:
-        total_ballet += 1
+        total_ballet += 1       #adding total of votes
 
-        if row[2] in candidate_dict:
-            candidate_dict[row[2]] = candidate_dict[row[2]] + 1
+        if row[2] in candidate_dict:        #using in dictionary so we can store and comparre each value
+            candidate_dict[row[2]] = candidate_dict[row[2]] + 1  #using the adding to total the number of votes/rows associated with the name
         else:
             candidate_dict[row[2]] = 1
     
-    print("Election Results")
+    print("Election Results")               #printing before for loop
     print("-------------------")
     print(f"Total Votes: {total_ballet}")
     print("-------------------")
     
-    most_votes = 0
+    most_votes = 0      #setting p values
     most_who = ""
 
-    candidate_info = []
+    candidate_info = []    #creating list to store info from for loop
         
-    for key, value in candidate_dict.items():
+    for key, value in candidate_dict.items():   #used .items() to retain and seperate values to use later
         
-        print(f'{key}: {value} ({(int(value)/total_ballet):.3%})')
-        candidate_info.append("{}".format(f'{key}: {value} ({(int(value)/total_ballet):.3%})') + os.linesep)
+        print(f'{key}: {value} ({(int(value)/total_ballet):.3%})')      #:.3% instead of value / total * 100  #Also printing in for loop to get each canidate info
+                                                                     
+        candidate_info.append("{}".format(f'{key}: {value} ({(int(value)/total_ballet):.3%})') + os.linesep)    #os.linesep to try and give a new row
         
-        if most_votes == 0:
+        if most_votes == 0:         #using a comparison to find most votes
             most_votes = value
             most_who = key
 
-        if value > most_votes:
+        if value > most_votes:      #using previous finding to get name(key) related to value
             most_votes = value
             most_who = key
 
-print("-------------------")
+print("-------------------")    #print next set including value just gained
 print(f'Winner: {most_who}')            
 print("-------------------")
 
-print(candidate_info)
-
-
-output_path = os.path.join("analysis", "Election_Results.txt")
+output_path = os.path.join("analysis", "Election_Results.txt") #writing a new .txt file
 
 with open(output_path, 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
 
     csvwriter.writerow(["Election Results"])
     csvwriter.writerow(["-------------------"])
-    csvwriter.writerow(["Total Votes:", f"{total_ballet}"])
+    csvwriter.writerow(["Total Votes:", f"{total_ballet}"]) #used f string at the end so I could seperate info better
     csvwriter.writerow(["-------------------"])
-    csvwriter.writerow(candidate_info)
+    csvwriter.writerow(candidate_info)          #thinking I should have used a def to make a function, but found this way easier to grasp
     csvwriter.writerow(["-------------------"])
     csvwriter.writerow(["Winner:" f'{most_who}'])
     csvwriter.writerow(["-------------------"])
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Help from Tutor Khangwelo with finding my Change variables in the code next_chan
     Also asked about an easy way to round down using .2f
 Xpert showed me how to seperate the csv output without a "," after every letter
     (I didn't realize I could use an f string in the middle and not just the beginning)
+
+Help from Tutor Reza with using dictionary to save values from a loop, candidate_dict, and reminding me about the "in" for an if statement
+Googled ways to move to new row for list, easy percent change with :.3%
